a5c685613fbcddaca056fe8a21e66155a8e72607
arabmistcoffeegenie <shahulsauri@gmail.com>
2025-04-06 19:22:27 +0100

    Add read.php to view saved form submissions

--- read.php
+++ read.php
@@ -1,3 +1,10 @@
 <?php
-  echo nl2br(file_get_contents("data.txt"));
+  echo "<h3>Saved Submissions:</h3>";
+  $dataFile = '/tmp/data.txt';
+
+  if (file_exists($dataFile)) {
+    echo "<pre>" . htmlspecialchars(file_get_contents($dataFile)) . "</pre>";
+  } else {
+    echo "No data yet.";
+  }
 ?>
